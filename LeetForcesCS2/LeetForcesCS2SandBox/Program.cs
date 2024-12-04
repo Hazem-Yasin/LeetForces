68754e0eb642e43c0ca04b9822f946666ce7feb3
Hazem Yasin <hazem.yasin.1416@gmail.com>
2024-12-04 21:18:06 +0200

    still solving the second issue in 75

--- LeetForcesCS2/LeetForcesCS2SandBox/Program.cs
+++ LeetForcesCS2/LeetForcesCS2SandBox/Program.cs
@@ -1,14 +1,3 @@
-﻿//leetcode 75 
-//1768. Merge String Alternatly 
-string word1 = "abc";
-string word2 = "def";
-string word3 = "";
+﻿string str1 = "";
+string str2 = "";
 
-for (int x = 0; x < word1.Length; x++){
-    word3 += word1[x];
-}
-for (int y = 0; y < word2.Length; y++){
-word3 += word2[y];
-}
-
-Console.WriteLine(word3);
